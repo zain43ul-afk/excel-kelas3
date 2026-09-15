d60d3c7c2da11c0aa5c885d8982fd17f6f13b341
zain43ul-afk <zain43ul@gmail.com>
2026-09-15 14:02:53 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAcZQCmSNHf_CNaj6F48ltJ5vFA7vvIMRY",
+  authDomain: "excel-kelas3.firebaseapp.com",
+  databaseURL: "https://excel-kelas3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "excel-kelas3",
+  storageBucket: "excel-kelas3.firebasestorage.app",
+  messagingSenderId: "G-JLBQB2Z97Z",
+  appId: "1:49142425536:web:401b90ce6c9ca76bf90a25"
+};
